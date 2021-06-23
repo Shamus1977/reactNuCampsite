5d15fa5dc0a0856fc38ac6126976bbe70d644210
SHJohnson <shjohnson413@gmail.com>
2021-06-22 20:49:28 -0700

    transformed stateless components to function components.

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,24 +1,23 @@
 
-import React, {Component}  from 'react';
+import React from 'react';
 import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
 
-class CampsiteInfo extends Component{
 
-    renderCampsite(campsite){
+   function RenderCampsite({campsite}){
         return (
             <div className='col-md-5 m-1'>
                 <Card>
-                        <CardImg top src={campsite.image} alt={campsite.name} />
-                        <CardBody>
-                            <CardTitle>{campsite.name}</CardTitle>
-                            <CardText>{campsite.description}</CardText>
-                        </CardBody>
-                    </Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                    <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
             </div>
         )
     }
 
-    renderComments(comments){
+    function RenderComments({comments}){
         if(comments){
             return (
                 <div className='col-md-5 m-1'>
@@ -37,13 +36,13 @@ class CampsiteInfo extends Component{
 
  
 
-    render(){
-        if(this.props.campsite){
+    function CampsiteInfo(props){
+        if(props.campsite){
             return ( 
                 <div className='container'>
                     <div className='row'>
-                    {this.renderCampsite(this.props.campsite)} 
-                    {this.renderComments(this.props.campsite.comments)}
+                    <RenderCampsite campsite ={props.campsite} />
+                    <RenderComments comments = {props.campsite.comments} />
                     </div>
                 </div>
             );
@@ -51,7 +50,5 @@ class CampsiteInfo extends Component{
             return <div />;
         }
     }
-
-}
 
 export default CampsiteInfo;
