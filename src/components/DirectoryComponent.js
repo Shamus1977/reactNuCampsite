fc86672ad41f961845ab53773402725085c1ab9c
SHJohnson <shjohnson413@gmail.com>
2021-06-25 00:01:35 -0700

    added breadcrumbs

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,14 +1,17 @@
 import React  from 'react';
-import {Card, CardImg, CardImgOverlay, CardTitle} from 'reactstrap';
+import {Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
     function RenderDirectoryItem({campsite}) {
       return (
         <Card>
-            <CardImg src={campsite.image} alt={campsite.name} />
-            <CardImgOverlay>
-                <CardTitle>{campsite.name}</CardTitle>
-            </CardImgOverlay>
+            <Link to={`/directory/${campsite.id}`}>
+                <CardImg src={campsite.image} alt={campsite.name} />
+                <CardImgOverlay>
+                    <CardTitle>{campsite.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
         </Card>
       );
     }
@@ -25,6 +28,16 @@ import {Card, CardImg, CardImgOverlay, CardTitle} from 'reactstrap';
 
         return (
             <div className='container'>
+                <div className='row'>
+                    <div className='col'>
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                            <BreadcrumbItem active >Directory</BreadcrumbItem>
+                        </Breadcrumb>
+                        <h2>Directory</h2>
+                        <hr />
+                    </div>
+                </div>
                 <div className='row'>
                     { directory }
                 </div>
